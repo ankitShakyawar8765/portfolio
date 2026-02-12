2f146215f868f823bdb6289423cc26fe686b20ee
ankit babu <it166341@gmail.com>
2026-02-12 14:42:41 +0530

    fix: add .nojekyll file and update deployment config for GitHub Pages caching issue

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [ tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
 })

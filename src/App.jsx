a909c2d335901e47cea046900038500e0c879ea1
ankit babu <it166341@gmail.com>
2026-02-12 14:28:06 +0530

    fixed app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,42 @@
-import { useState } from 'react'
-import Navbar from './component/Navbar';
-import Home from './section/Home';
-import About from './section/About';
-import Skills from './section/Skills';
-import Project from './section/Project';
-import Experience from './section/Experience';
-import Testomonial from './section/Testomonial';
-import Contact from './section/Contact';
-import Footer from './section/Footer';
-// import Background from './component/Background';
-import CustomCursor from './component/CustomCursor';
-import IntroAnimation from './component/IntroAnimation';
-import React from 'react';
+import React from 'react'
+import Navbar from './component/Navbar'
+import Home from './section/Home'
+import About from './section/About'
+import Skills from './section/Skills'
+import Project from './section/Project'
+import Experience from './section/Experience'
+import Testomonial from './section/Testomonial'
+import Contact from './section/Contact'
+import Footer from './section/Footer'
+import CustomCursor from './component/CustomCursor'
+import IntroAnimation from './component/IntroAnimation'
 
-
-    function App() {
-    const [introDone , setIntroDone] = React.useState(false);
- 
+function App() {
+  const [introDone, setIntroDone] = React.useState(false)
 
   return (
     <>
-    {!introDone && <IntroAnimation onFinish={() => setIntroDone(true)} />}
+      {!introDone && (
+        <IntroAnimation onFinish={() => setIntroDone(true)} />
+      )}
 
       {introDone && (
-     <div className='relative gradient text-white'>
-
-      
-      <CustomCursor/>
-      {/* <Background/> */}
-     <Navbar/>
-    <Home/>
-    <About/>
-    <Skills/>
-    <Project/>
-    <Experience/>
-    <Testomonial/>
-    <Contact/>
-   <Footer/>
-     </div>
-     )}
-     
+        <div className='relative gradient text-white'>
+          <CustomCursor />
+          <Navbar />
+          <Home />
+          <About />
+          <Skills />
+          <Project />
+          <Experience />
+          <Testomonial />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </>
   )
 }
 
 export default App
+ 
